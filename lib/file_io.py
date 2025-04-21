e7c1e62cde456cf27f5068e310cefd20e95b7a5d
DavidSoi96 <david.soi@student.moringaschool.com>
2025-04-21 18:06:06 +0300

    assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    path = Path(f"{file_name}.txt")
+    with path.open('w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    path = Path(f"{file_name}.txt")
+    with path.open('a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    path = Path(f"{file_name}.txt")
+    with path.open('r') as file:
+        return file.read()
+
+
+        
